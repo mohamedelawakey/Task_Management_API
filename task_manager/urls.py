1581fa8c8fd44366e43dba8e3f461a3b4ec31d14
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-08-13 13:07:20 +0300

    Register Task model in admin with list display, filters, and search

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from tasks.views import TaskViewSet, UserViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # JWT
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # API
+    path('api/', include(router.urls)),
 ]
